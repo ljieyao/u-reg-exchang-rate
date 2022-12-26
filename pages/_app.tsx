a377a0e379bec2733856429dd19eacb7447cbdbc
JY Liu <ljieyao0210@gmail.com>
2022-12-26 23:38:23 +0800

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { NoSsr, ThemeProvider } from "@mui/material";
+import { theme } from "../theme";
+import { NextPage } from "next";
+import Head from "next/head";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
+const URegCurrencyRateApp: NextPage<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <NoSsr>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <ThemeProvider theme={theme}>
+          <Head>
+            <title>U-Reg Currency Converter</title>
+          </Head>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </LocalizationProvider>
+    </NoSsr>
+  );
+};
+
+export default URegCurrencyRateApp;
