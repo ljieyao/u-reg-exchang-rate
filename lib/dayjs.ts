46cf806ab8a3721de7e1283743153aea11097c18
JY Liu <ljieyao0210@gmail.com>
2022-12-27 11:49:33 +0800

    extend dayjs

---
+++ lib/dayjs.ts
@@ -0,0 +1,4 @@
+import dayjs from "dayjs";
+import isToday from "dayjs/plugin/isToday";
+
+dayjs.extend(isToday);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import "../lib/dayjs";
+
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { NoSsr, ThemeProvider } from "@mui/material";
